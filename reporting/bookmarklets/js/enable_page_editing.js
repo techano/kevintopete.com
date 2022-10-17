0637c3d0170cac2621a2aceffaa77cc0295a4458
techano <kevin.topete.kt@protonmail.com>
2022-10-17 09:06:58 -0700

    test minify

--- reporting/bookmarklets/js/enable_page_editing.js
+++ reporting/bookmarklets/js/enable_page_editing.js
@@ -1,9 +1,9 @@
 function confirmbox() {
-    if (confirm("This script is only meant to be used for reporting purposes to Gilbert Public Schools, not for student use. Click OK to continue running script. This will work on any site.")) {
+    if (confirm('This script is only meant to be used for reporting purposes to Gilbert Public Schools, not for student use. Click OK to continue running script. This will work on any site.')) {
         javascript:document.body.contentEditable = 'true'; document.designMode='on'; void 0
-        window.alert("Script has been run. Click anywhere to edit text");
+        window.alert('Script has been run. Click anywhere to edit text');
     } else {
-        window.alert("Script Terminated");
+        window.alert('Script Terminated');
     }
 }
 
